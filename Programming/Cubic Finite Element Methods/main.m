528bc4cccd6f4132680486ad99b1dea054dd80cf
tqlorz <3097714673@qq.com>
2025-11-22 20:10:41 +0800

    sort the program of quadratic and cubic FEM

--- Programming/Cubic Finite Element Methods/main.m
+++ Programming/Cubic Finite Element Methods/main.m
@@ -2,12 +2,12 @@ clc;
 clear;
 
 %% Set parameters 
-h = pow2(-2);
+h = pow2(-1);
 f = @(x) 2 * pi^2 .* sin(pi * x(:, 1)) .* sin(pi * x(:, 2));
 
 %% Solve FEM problem
-[node, elem, edge, elem2dof, u_h] = solveFEM(h, f);
-allnode = getallnode(node, edge, elem2dof);
+[node, elem, edge, elem2dof, u_h] = solveFEM_P3(h, f);
+allnode = getallnode_P3(node, elem, edge, elem2dof);
 
 %% Draw picture of mesh
 figure(1);
@@ -15,8 +15,7 @@ hold on;
 axis on;
 showmesh(node, elem);
 findelem(node, elem);       % plot indices of all triangles
-findnode(node);             % plot indices of all vertices
-findedgedof(node, edge);    % plot indices of all edge
+findnode(allnode);             % plot indices of all vertices
 
 %% Draw picture of FEM
 [Xi, Yi] = meshgrid(linspace(0, 1, 100), ...
@@ -37,44 +36,33 @@ title('3D Surface Plot of u_h');
 xlabel('X'); ylabel('Y'); zlabel('u_h');
 
 %% Verify convergence in H1-norm
-% h = pow2(-2);
-% err = zeros(4, 1);
-% N = zeros(4, 1);
-% Du = @(x) [pi * cos(pi * x(:, 1)) .* sin(pi * x(:, 2))
-%            pi * sin(pi * x(:, 1)) .* cos(pi * x(:, 2))];
-% for k = 1 : 4
-%     [node, elem, u_h] = solveFEM(h, f);
-%     N(k) = size(node, 1);
-%     err(k) = getH1error(node, elem, Du, u_h);
-%     h = h / 2;
-% end
-% figure(3);
-% showrate(N,err);
+[node, elem] = squaremesh([0, 1, 0, 1], 0.25);
+for k = 1 : 4
+    exactu = inline('sin(pi*pxy(:,1)).*sin(pi*pxy(:,2))','pxy');
+    Du = inline('[pi*cos(pi*pxy(:,1)).*sin(pi*pxy(:,2)) pi*sin(pi*pxy(:,1)).*cos(pi*pxy(:,2))]','pxy');
+    [elem2dof, elem2edge, edge, bdDof] = dofP3(elem);
+    allnode = getallnode_P3(node, elem, edge, elem2dof);
+    uI = exactu(allnode);
+    N(k) = size(node, 1);
+    err(k) = getH1error(node, elem, Du, uI);
+    [node, elem] = uniformrefine(node, elem);
+end
+figure(4);
+showrate(N, err);
 
 %% Verify convergence in L2-norm
-% h = pow2(-1);
-% err = zeros(4,1);
-% N = zeros(4,1);
-% exactu = @(x) sin(pi * x(:, 1)) .* sin(pi * x(:, 2));
-% for k = 1 : 4 
-%     [node, elem, edge, elem2dof, u_h] = solveFEM(h, f);
-%     uI = exactu([node; (node(edge(:,1),:)+node(edge(:,2),:))/2]);
-%     N(k) = 1/sqrt(size(elem, 1));
-%     err(k) = getL2error(node, elem, exactu, u_h);
-%     h = h / 2;
-% end
-% figure(4);
-% showrateh(N, err);
+exactu = @(x) sin(pi * x(:, 1)) .* sin(pi * x(:, 2));
+[node, elem] = squaremesh([0, 1, 0, 1], 0.25);
+err = zeros(4, 1); N = zeros(4, 1);
+for k = 1 : 4
+    [node, elem] = uniformrefine(node, elem);
+    [elem2dof, elem2edge, edge, bdDof] = dofP3(elem);
+    allnode = getallnode_P3(node, elem, edge, elem2dof);
+    uI = exactu(allnode);
+    N(k) = 1 / sqrt(size(elem, 1));
+    err(k) = getL2error(node, elem, exactu, uI);
+end
+figure(5);
+showrateh(N, err);
 
-% pde = Stokesdata2;
-% [node,elem] = squaremesh([0,1,0,1],0.25);
-% err = zeros(4,1); h = zeros(4,1);
-% for k = 1:4
-%     [node,elem] = uniformrefine(node,elem);
-%     [elem2edge,edge] = dofedge(elem);
-%     uI = pde.exactu([node; (node(edge(:,1),:)+node(edge(:,2),:))/2]);
-%     h(k) = 1/sqrt(size(elem,1));
-%     err(k) = getL2error(node,elem,pde.exactu,uI);
-% end
-% showrateh(h,err);
 
--- Programming/Quadratic Finite Element Methods/main.m
+++ Programming/Quadratic Finite Element Methods/main.m
@@ -6,8 +6,8 @@ h = pow2(-2);
 f = @(x) 2 * pi^2 .* sin(pi * x(:, 1)) .* sin(pi * x(:, 2));
 
 %% Solve FEM problem
-[node, elem, edge, elem2dof, u_h] = solveFEM_P3(h, f);
-allnode = getallnode_P3(node, elem, edge, elem2dof);
+[node, elem, edge, elem2dof, u_h] = solveFEM(h, f);
+allnode = getallnode(node, edge, elem2dof);
 
 %% Draw picture of mesh
 figure(1);
@@ -15,7 +15,8 @@ hold on;
 axis on;
 showmesh(node, elem);
 findelem(node, elem);       % plot indices of all triangles
-findnode(allnode);             % plot indices of all vertices
+findnode(node);             % plot indices of all vertices
+findedgedof(node, edge);    % plot indices of all edge
 
 %% Draw picture of FEM
 [Xi, Yi] = meshgrid(linspace(0, 1, 100), ...
@@ -40,8 +41,8 @@ xlabel('X'); ylabel('Y'); zlabel('u_h');
 for k = 1 : 4
     exactu = inline('sin(pi*pxy(:,1)).*sin(pi*pxy(:,2))','pxy');
     Du = inline('[pi*cos(pi*pxy(:,1)).*sin(pi*pxy(:,2)) pi*sin(pi*pxy(:,1)).*cos(pi*pxy(:,2))]','pxy');
-    [elem2dof, elem2edge, edge, bdDof] = dofP3(elem);
-    allnode = getallnode_P3(node, elem, edge, elem2dof);
+    [elem2dof, elem2edge, edge, bdDof] = dofP2(elem);
+    allnode = getallnode_P2(node, elem, edge, elem2dof);
     uI = exactu(allnode);
     N(k) = size(node, 1);
     err(k) = getH1error(node, elem, Du, uI);
@@ -56,13 +57,11 @@ exactu = @(x) sin(pi * x(:, 1)) .* sin(pi * x(:, 2));
 err = zeros(4, 1); N = zeros(4, 1);
 for k = 1 : 4
     [node, elem] = uniformrefine(node, elem);
-    [elem2dof, elem2edge, edge, bdDof] = dofP3(elem);
-    allnode = getallnode_P3(node, elem, edge, elem2dof);
+    [elem2dof, elem2edge, edge, bdDof] = dofP2(elem);
+    allnode = getallnode_P2(node, elem, edge, elem2dof);
     uI = exactu(allnode);
     N(k) = 1 / sqrt(size(elem, 1));
     err(k) = getL2error(node, elem, exactu, uI);
 end
 figure(5);
 showrateh(N, err);
-
-
